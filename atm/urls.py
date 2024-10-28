d79cd92b8d6ffb5cbe22e8dea860265986cdd72a
Z321T <fzc19942496944@outlook.com>
2024-10-28 16:54:18 +0800

    更新了atm的路由

--- atm/urls.py
+++ atm/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.staticfiles.views import serve
+from .views import login
 
 app_name = 'atm'
 
 urlpatterns = [
 
     path('static/<path:path>', serve),
+    path('login/', login, name='login'),
 
 ]
